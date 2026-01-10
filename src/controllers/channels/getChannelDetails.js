5607a09dce78a758178c23a9d35f9bc94eaf531b
gaurav <gauravg77061@gmail.com>
2026-01-11 02:36:38 +0530

    getting  real data fromdb for channel route by id

--- src/controllers/channels/getChannelDetails.js
+++ src/controllers/channels/getChannelDetails.js
@@ -1,13 +1,33 @@
+
+const Channel= require('../../models/channel')
+
+const User=require('../../models/user');
+
 const getChannelDetails =async(req,res)=>{
     try {
 
+
+        const {channelId}=req.params;
+
+        const channel=await Channel.findById(channelId);
+
+        if(!channel){
+            return res.status(400).send('Channelnot found');
+        }
+
+        const user=await User.findOne({channel:channelId},{userName:1});
+
+        const streamUrl="http";
+
+        const isOnline=false;
+
         res.status(200).json({
-            id:1,
-            title:'channel',
-            description:'Hello',
-            userName: 'Gaurav',
-            isOnline:false,
-            streamUrl:'http'
+            id:channel._id,
+            title:channel.title,
+            description:channel.description,
+            userName: user.userName,
+            isOnline,
+            streamUrl
         })
         
     } catch (error) {
